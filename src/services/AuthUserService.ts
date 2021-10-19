d8b29681e3b61ccca1b27932b70950ffce1ca267
DeViktor <contato.4years@gmail.com>
2021-10-19 18:02:30 -0400

    first migration on SQLite

--- src/services/AuthUserService.ts
+++ src/services/AuthUserService.ts
@@ -8,12 +8,23 @@ import axios from "axios";
  * return token + infos user
  */
 
+interface AccessTokenResponse {
+    access_token: string
+}
+
+interface UserResponse {
+    avatar_url: string,
+    login: string,
+    id: number,
+    name: string
+}
+
 class AuthUserService {
     async exe(code: string) {
 
         const url = "https://github.com/login/oauth/access_token";
 
-        const response = await axios.post(url, null, {
+        const { data: AccessTokenResponse } = await axios.post<AccessTokenResponse>(url, null, {
             params: {
                 client_id: process.env.GITHUB_CLIENT_ID,
                 client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -22,7 +33,14 @@ class AuthUserService {
             headers: {
                 "Accept": "application/json"
             }
-        })
+        });
+
+        const response = await axios.get<UserResponse>("https://api.github.com/user", {
+            headers: {
+                authorization: `Bearer ${AccessTokenResponse.access_token}`
+            }
+        });
+
         return response.data;
     }
 }
