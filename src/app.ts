517d446c1b0fba5d87edd7a299c4a04f219989b0
DeViktor <contato.4years@gmail.com>
2021-10-18 23:27:11 -0400

    dot env instaled, github oauth started

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
-import express from "express";
+import express, { request } from "express";
 
 const app = express();
+
+app.get('/github', (request, response) => {
+    response.redirect(`https://github.com/login/oauth/autorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
+})
 
 app.listen(4000, () => console.log(`:rocket: server is running on port 4000`));
